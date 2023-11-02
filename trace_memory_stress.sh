88a5abe939076482b643c417198443bdcc6fe49f
JongHoB <qorwhdgh@gmail.com>
2023-11-02 15:57:16 +0000

    INIT trace_memory_stress.sh script

---
+++ trace_memory_stress.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Function to start tracing
+start_tracing() {
+	  echo "Starting tracing..."
+	    echo 1 > /sys/kernel/debug/tracing/tracing_on
+    }
+
+    # Function to stop tracing
+    stop_tracing() {
+	      echo "Stopping tracing..."
+	        echo 0 > /sys/kernel/debug/tracing/tracing_on
+	}
+
+	# Function to print trace data
+	print_trace_data() {
+		  echo "Trace Data:"
+		    cat /sys/kernel/debug/tracing/trace
+	    }
+
+	    # Start tracing
+	    start_tracing
+
+	    # Run stress-ng in the background
+	    stress-ng --vm 1 --vm-bytes 90% -t 10m &
+
+	    # Capture the process ID of the stress-ng command
+	    stress_ng_pid=$!
+
+	    # Wait for the stress-ng command to finish or be interrupted (Ctrl+C)
+	    wait $stress_ng_pid
+
+	    # Stop tracing
+	    stop_tracing
+
+	    # Print trace data
+	    print_trace_data
+
